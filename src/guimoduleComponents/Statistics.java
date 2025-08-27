dee4f75487a71d55aa1d482ebb413ddbad0cbe92
Dimitrios Georgiou <d.georgiou@edu.bbq.de>
2025-08-27 12:27:00 +0200

    Update Statistics.java

--- src/guimoduleComponents/Statistics.java
+++ src/guimoduleComponents/Statistics.java
@@ -26,9 +26,6 @@ import javax.swing.border.TitledBorder;
 <<<<<<< HEAD
  * @author D.Georgiou
  * @version 1.0
-=======
- * @author Quiz Application Team
- * @version 2.0
 >>>>>>> 51d430330dca283242d67944a6d45c96dfa445fd
  */
 public class Statistics extends JPanel implements Serializable {
